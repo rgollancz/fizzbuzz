b0d402e0ab6fbe7a814e87a5b7b5cbc79eb11fb0
Rosie <r.gollancz@gmail.com>
2016-09-19 13:45:15 +0100

    adds conditions to rspec

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,19 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "17" when passed "17"' do
+    expect(fizzbuzz(17)).to eq 17
+  end
+  it 'returns "fizz" when passed "6"' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+end
